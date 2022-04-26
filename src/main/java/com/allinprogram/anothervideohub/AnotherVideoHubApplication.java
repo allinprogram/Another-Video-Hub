6d9a6a07e1b6dabfea7781dfcde1c24cf9b44a8a
AllinProgram <ufan0@foxmail.com>
2022-04-26 19:24:03 +0800

    🎉Init

---
+++ src/main/java/com/allinprogram/anothervideohub/AnotherVideoHubApplication.java
@@ -0,0 +1,13 @@
+package com.allinprogram.anothervideohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherVideoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherVideoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allinprogram/anothervideohub/AnotherVideoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allinprogram.anothervideohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherVideoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
