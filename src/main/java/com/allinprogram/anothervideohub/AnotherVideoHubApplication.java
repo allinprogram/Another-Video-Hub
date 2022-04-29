05c36d391c0f3f6837da955b3abfce476b9bf31d
AllinProgram <ufan0@foxmail.com>
2022-04-29 19:51:49 +0800

    🚮 Del vaadin component

--- src/main/java/com/allinprogram/anothervideohub/AnotherVideoHubApplication.java
+++ src/main/java/com/allinprogram/anothervideohub/AnotherVideoHubApplication.java
@@ -2,13 +2,12 @@ package com.allinprogram.anothervideohub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
+
 @SpringBootApplication
 public class AnotherVideoHubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnotherVideoHubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AnotherVideoHubApplication.class, args);
+    }
 }
